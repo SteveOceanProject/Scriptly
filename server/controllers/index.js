ced74cfb510a4c1d190c92618635f68b4ddcde35
Will Atwood <x1@galaxia.us>
2022-05-09 00:35:18 -0400

    API routes setup and working.  added URL string for user photo and firebase videos.  Added a getAllUsers endpoint.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,31 +2,47 @@ const controller = require ('../database/controllers/scriptly.js')
 
 // eslint-disable-next-line no-undef
 module.exports = {
-  addUserFunction: (req, res) => {
-    controller.addUser(req.body)
-    .then(data => {
-      res.send(data)
-    })
-    .catch(err => console.log(err))
-  },
-  findOneSpeechFunction: (req, res) => {
-    controller.findOneSpeech(req.params)
+  getAllUsersFunction: (req, res) => {
+    const { page, count } = req.query
+    console.log("getAllUsersFunctions", page, count, req.query)
+    controller.getAllUsers(page, count)
     .then(data => res.send(data))
     .catch(err => res.send(err))
-  },
-  updateOneFunction: (req, res) => {
-    controller.updateOne(req.params, req.body)
-    .then(data => {
-      console.log('updateOneFunction ', data)
-      res.send(data)
-    })
-    .catch(err => console.log(err))
   },
   getUserDataFunction: (req, res) => {
     const params = req.params
     controller.getUserInfo(params)
     .then(data => res.send(data))
     .catch(err => res.send(err))
+  },
+  findOneSpeechFunction: (req, res) => {
+    controller.findOneSpeech(req.params)
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+  },
+  findUserSpeechesFunction: (req, res) => {
+    controller.findUserSpeeches(req.params)
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+  },
+  addUserFunction: (req, res) => {
+    controller.addUser(req.body)
+    .then(data => res.send(data))
+    .catch(err => console.log(err))
+  },
+  addSpeechFunction: (req, res) => {
+    controller.addSpeech(req.body)
+    .then(data => res.send(data))
+    .catch(err => res.send(err))
+
+  },
+  updateOneSpeechFunction: (req, res) => {
+    controller.updateOneSpeech(req.params, req.body)
+    .then(data => {
+      console.log('updateOneFunction ', data)
+      res.send(data)
+    })
+    .catch(err => console.log(err))
   },
 
 }
