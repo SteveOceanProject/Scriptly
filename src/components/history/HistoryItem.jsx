c176ed6de836e38d162609780c5638fabd6df840
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-05-10 13:38:59 -0600

    update history item functionality

--- src/components/history/HistoryItem.jsx
+++ src/components/history/HistoryItem.jsx
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import DoughnutChart from '../charts/DoughnutChart.jsx';
+import {resultsModal} from '../../atoms.jsx';
+import {useRecoilState} from 'recoil';
+import Results from '../../results/Results.jsx';
 
 function HistoryItem(props) {
   const { date, body, analysis } = props;
+  const [showResults, setShowResults] = useRecoilState(resultsModal);
   let bodyPreview = body.slice(0, 150);
 
+  console.log('inside result', showResults);
+
   return (
-    <li className="speech-history-list-item">
+    <li className="speech-history-list-item" onClick={()=> { setShowResults(true) }}>
       <small className="speech-date">{date}</small>
       <div className="speech-preview-chart">
         <p className="speech-preview-body">{`${bodyPreview}...`}</p>
